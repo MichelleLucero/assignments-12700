f92cad7be7f45b9810224c0f4d8dbb09c44a9498
mlucero4607 <michelle.lucero07@myhunter.cuny.edu>
2017-09-24 22:42:29 -0400

    adding comma code

---
+++ 04/commacode.py
@@ -0,0 +1,15 @@
+spam = ['apples', 'bananas', 'tofu', 'cats']
+
+
+def comma(list):
+    for i in range(len(list) -2):
+        print(list[i] + ', ', end="")
+    print(list[-2] + ', and ' + list[-1]) 
+
+    
+
+   
+    
+             
+            
+    
