8e7e471d99d6d410b2a40c3827749b373ec230ce
mlucero4607 <michelle.lucero07@myhunter.cuny.edu>
2017-10-11 22:51:31 -0400

    fixed

--- 07/cipher.py
+++ 07/cipher.py
@@ -1,6 +1,6 @@
 
 import math
-real_stats = [0.08167,0.01492,0.02782,0.04253,0.12702,0.02228,0.02015,0.06094,0.06966,0.00153,0.00772,0.04025,0.02406,0.06749,0.07507,0.01929,0.00095,0.05987,0.06327,0.09056,0.02758,0.00978,0.02360,0.00150,0.01974,0.00074]
+
 
 def rotate_char(c,r):
     """
@@ -81,9 +81,11 @@ def decode(s):
     return small
                       
 
-f = open('pg1661.txt').read()
-w = f.lower()
-real_stats = build_frequency_vector(w)
+f = open('pg1661.txt')
+r= f.read()
+r = r.lower()
+real_stats = build_frequency_vector(r)
+f.close()
 
 s = 'Living that bum life'
 print ("encoded sentence: " + encode_string(s,9))
