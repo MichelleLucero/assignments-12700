02e771f4ea376e3751cbc3626828fdc47e9801ca
mlucero4607 <michelle.lucero07@myhunter.cuny.edu>
2017-11-15 22:51:46 -0500

    Practice Problem #2

--- 10/atbash.py
+++ 10/atbash.py
@@ -1,3 +1,11 @@
+"""
+The Atbash cipher is a simple substitution cipher that
+relies on transposing all the lettersin the alphabet such
+that the resulting alphabet is backwards. The first letter is
+replaced with the last letter, the second with the second-last,
+and so on.
+"""
+
 
 w = 'almnobc'
 
